b287379411f9a9b7f6f486cf2cd9f723d401ba1f
Cory Sturgis-Casturgi <casturgi@gmail.com>
2015-04-29 22:18:30 -0500

    fixed minor things

--- js/app.js
+++ js/app.js
@@ -19,14 +19,14 @@ function fizzbuzz(num){
 	}
 	else {
 		for(var i = 1; i <= num; i++){
-			if (i % 3 == 0){
-				$('.numbers').append('<li>Fizz</li>');
+			if (i % 15 == 0){
+				$('.numbers').append('<li>FIZZBUZZ!</li>');
 			}
 			else if (i % 5 == 0){
 				$('.numbers').append('<li>Buzz</li>');
 			}
-			else if (i % 15 == 0){
-				$('.numbers').append('<li>FIZZBUZZ!</li>');
+			else if (i % 3 == 0){
+				$('.numbers').append('<li>Fizz</li>');
 			}
 			else {
 				$('.numbers').append('<li>' + i + '</li>');
